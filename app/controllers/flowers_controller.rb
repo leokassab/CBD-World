0f89561fa3fa5689eb4348033a3fd2c5529ac0ec
Christelle Lecamp <christelle.lecamp@gmail.com>
2022-01-08 16:38:28 +0100

    ds flowers controller index show new create

--- app/controllers/flowers_controller.rb
+++ app/controllers/flowers_controller.rb
@@ -1,2 +1,29 @@
 class FlowersController < ApplicationController
+  def index
+    @flowers = Flower.all
+  end
+
+  def show
+    set_flower
+  end
+
+  def new
+    @flower = Flower.new
+  end
+
+  def create
+    @flower = Flower.new(flower_params)
+    @flower.save
+    redirect_to flower_path(@flower)
+  end
+
+private
+
+def flower_params
+  params.require(:flower).permit(:name, :price, :country, :type , :description, :cbd_ratio, :effect, :flavor, :weight)
+end
+
+def set_flower
+  @flower = Flower.find(params[:id])
+end
 end
