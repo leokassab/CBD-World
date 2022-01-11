b47c9242fbb0cdfac18f447cae698064453e45f2
theo denis <theo.laos2@gmail.com>
2022-01-11 20:32:53 +0100

    crappy index added v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first
-
-Flower.new(name: "test", price: "3", description: "lorem ipsum")
