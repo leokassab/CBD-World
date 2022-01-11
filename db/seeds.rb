9f801bdf388afb63eccf93a84600e19055aea09b
theo denis <theo.laos2@gmail.com>
2022-01-11 20:34:13 +0100

    crappy index added v2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first
+require  'faker'
+
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating users ..."
+20.times do
+    user = User.create!(
+    email: Faker::Internet.email,
+    password: 123456,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone_number: rand(600000000..699999999),
+    adress: Faker::Address.street_address,
+    zip_code: Faker::Address.zip_code,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    merchant: true,
+    vat_number: rand(0..100),
+    kbis: rand(0..200),
+    siret: rand(0..1000),
+    compagny_name: Faker::Company.name
+  )
+    puts "Created #{user.last_name}"
+end
+20.times do
+    user = User.create!(
+    email: Faker::Internet.email,
+    password: 123456,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    phone_number: rand(600000000..699999999),
+    adress: Faker::Address.street_address,
+    zip_code: Faker::Address.zip_code,
+    city: Faker::Address.city,
+    country: Faker::Address.country,
+    merchant: false,
+  )
+    puts "Created #{user.last_name}"
+end
+
+
+puts "Finished!"
