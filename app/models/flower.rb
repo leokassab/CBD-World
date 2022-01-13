0a8d9a07ee427debf6501b3fc7bf9f1a77229d3b
theo denis <theo.laos2@gmail.com>
2022-01-13 20:06:16 +0100

    up to date

--- app/models/flower.rb
+++ app/models/flower.rb
@@ -1,5 +1,6 @@
 class Flower < ApplicationRecord
   has_many :orders
+  has_many :reviews
 
   validates :name, presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :orders
+  has_many :reviews
+  validates :email, :phone_number, uniqueness: true, presence: true
+  validates :adress, :first_name, :last_name, presence: true
 end
