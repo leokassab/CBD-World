7323af0af4c57f655879e46e47e6634a918ef14d
theo denis <theo.laos2@gmail.com>
2022-01-15 12:40:35 +0100

    orders are good

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
 
   def index
-    my_orders = Order.where(user: current_user)
+    @orders = Order.where(user: current_user)
   end
 
   def show
@@ -16,6 +16,28 @@ class OrdersController < ApplicationController
     my_order
     @order.destroy
     redirect_to orders_path
+  end
+
+  def new
+    @flower = Flower.find(params[:flower_id])
+    @user = current_user
+    @order = Order.new
+
+  end
+
+  def create
+    @flower = Flower.find(params[:flower_id])
+    order = Order.new(paid: true, flower_id: @flower.id, user_id: current_user.id)
+    if order.save
+      redirect_to orders_path
+   else
+     render :new
+   end
+
+  def destroy
+
+  end
+
   end
 
   private
