0e30e11f5e3cd26d180358815e52dd69dc0c130b
elliott gabison <elliott.gabison@gmail.com>
2022-01-18 21:07:56 +0100

    ajout des fichiers photos + reprise des seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -108,7 +108,7 @@ MERCHANT_USER = MERCHANTS.sample
 BUYER_USER = BUYERS.sample
 
 
-dragon_arm = Flower.create(
+dragon_arm = Flower.new(
   name: "Dragon Arm",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -120,7 +120,12 @@ dragon_arm = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-dragon_fly = Flower.create(
+dragon_arm_path = Rails.root.join('app', 'assets', 'images', 'dragon_arm.jpeg')
+dragon_arm_photo = File.open(dragon_arm_path)
+dragon_arm.photo.attach(io: dragon_arm_photo, filename: 'dragon_arm.jpeg', content_type: 'image/jpeg')
+dragon_arm.save!
+
+dragon_fly = Flower.new(
   name: "Dragon Fly",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -132,7 +137,12 @@ dragon_fly = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-white_widow = Flower.create(
+dragon_fly_path = Rails.root.join('app', 'assets', 'images', 'dragon_fly.jpeg')
+dragon_fly_photo = File.open(dragon_fly_path)
+dragon_fly.photo.attach(io: dragon_fly_photo, filename: 'dragon_fly.jpeg', content_type: 'image/jpeg')
+dragon_fly.save!
+
+white_widow = Flower.new(
   name: "White Widow",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -144,7 +154,12 @@ white_widow = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-dragon_kush = Flower.create(
+white_widow_path = Rails.root.join('app', 'assets', 'images', 'white_widow.jpeg')
+white_widow_photo = File.open(white_widow_path)
+white_widow.photo.attach(io: white_widow_photo, filename: 'white_widow.jpeg', content_type: 'image/jpeg')
+white_widow.save!
+
+dragon_kush = Flower.new(
   name: "Dragon Kush",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -156,7 +171,12 @@ dragon_kush = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-amnesia = Flower.create(
+dragon_kush_path = Rails.root.join('app', 'assets', 'images', 'dragon_kush.jpeg')
+dragon_kush_photo = File.open(dragon_kush_path)
+dragon_kush.photo.attach(io: dragon_kush_photo, filename: 'dragon_kush.jpeg', content_type: 'image/jpeg')
+dragon_kush.save!
+
+amnesia = Flower.new(
   name: "Amnésia",
   price: Faker::Commerce.price(range: 30.0..60.0),
   country: Faker::Address.country,
@@ -168,7 +188,12 @@ amnesia = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 3),
   user: MERCHANT_USER
 )
-gorilla_glue = Flower.create(
+amnesia_path = Rails.root.join('app', 'assets', 'images', 'amnesia.jpeg')
+amnesia_photo = File.open(amnesia_path)
+amnesia.photo.attach(io: amnesia_photo, filename: 'amnesia.jpeg', content_type: 'image/jpeg')
+amnesia.save!
+
+gorilla_glue = Flower.new(
   name: "Gorilla Glue",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -180,7 +205,12 @@ gorilla_glue = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-heavy_og = Flower.create(
+gorilla_glue_path = Rails.root.join('app', 'assets', 'images', 'gorilla_glue.jpeg')
+gorilla_glue_photo = File.open(gorilla_glue_path)
+gorilla_glue.photo.attach(io: gorilla_glue_photo, filename: 'gorilla_glue.jpeg', content_type: 'image/jpeg')
+gorilla_glue.save!
+
+heavy_og = Flower.new(
   name: "Heavy OG",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -192,7 +222,12 @@ heavy_og = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-cookie_dough = Flower.create(
+heavy_og_path = Rails.root.join('app', 'assets', 'images', 'heavy_og.jpeg')
+heavy_og_photo = File.open(heavy_og_path)
+heavy_og.photo.attach(io: heavy_og_photo, filename: 'heavy_og.jpeg', content_type: 'image/jpeg')
+heavy_og.save!
+
+cookie_dough = Flower.new(
   name: "Cookie dough",
   price: Faker::Commerce.price(range: 100.0..200.0),
   country: Faker::Address.country,
@@ -204,7 +239,12 @@ cookie_dough = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 10),
   user: MERCHANT_USER
 )
-og_salad = Flower.create(
+cookie_dough_path = Rails.root.join('app', 'assets', 'images', 'cookie_dough.jpeg')
+cookie_dough_photo = File.open(cookie_dough_path)
+cookie_dough.photo.attach(io: cookie_dough_photo, filename: 'cookie_dough.jpeg', content_type: 'image/jpeg')
+cookie_dough.save!
+
+og_salad = Flower.new(
   name: "OG Salad",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -216,7 +256,12 @@ og_salad = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-kalifa_kush = Flower.create(
+og_salad_path = Rails.root.join('app', 'assets', 'images', 'og_salad.jpeg')
+og_salad_photo = File.open(og_salad_path)
+og_salad.photo.attach(io: og_salad_photo, filename: 'og_salad.jpeg', content_type: 'image/jpeg')
+og_salad.save!
+
+kalifa_kush = Flower.new(
   name: "Kalifa Kush",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -228,7 +273,12 @@ kalifa_kush = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-watermelon_kush = Flower.create(
+kalifa_kush_path = Rails.root.join('app', 'assets', 'images', 'kalifa_kush.jpeg')
+kalifa_kush_photo = File.open(kalifa_kush_path)
+kalifa_kush.photo.attach(io: kalifa_kush_photo, filename: 'kalifa_kush.jpeg', content_type: 'image/jpeg')
+kalifa_kush.save!
+
+watermelon_kush = Flower.new(
   name: "Watermelon Kush",
   price: Faker::Commerce.price(range: 100.0..200.0),
   country: Faker::Address.country,
@@ -240,7 +290,12 @@ watermelon_kush = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 10),
   user: MERCHANT_USER
 )
-critical_kush = Flower.create(
+watermelon_kush_path = Rails.root.join('app', 'assets', 'images', 'watermelon_kush.jpeg')
+watermelon_kush_photo = File.open(watermelon_kush_path)
+watermelon_kush.photo.attach(io: watermelon_kush_photo, filename: 'watermelon_kush.jpeg', content_type: 'image/jpeg')
+watermelon_kush.save!
+
+critical_kush = Flower.new(
   name: "Critical Kush",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -252,7 +307,12 @@ critical_kush = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-lemonade_kush = Flower.create(
+critical_kush_path = Rails.root.join('app', 'assets', 'images', 'critical_kush.jpeg')
+critical_kush_photo = File.open(critical_kush_path)
+critical_kush.photo.attach(io: critical_kush_photo, filename: 'critical_kush.jpeg', content_type: 'image/jpeg')
+critical_kush.save!
+
+lemonade_kush = Flower.new(
   name: "Lemonade Kush",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -264,7 +324,12 @@ lemonade_kush = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-mojito_kush = Flower.create(
+lemonade_kush_path = Rails.root.join('app', 'assets', 'images', 'lemonade_kush.jpeg')
+lemonade_kush_photo = File.open(lemonade_kush_path)
+lemonade_kush.photo.attach(io: lemonade_kush_photo, filename: 'lemonade_kush.jpeg', content_type: 'image/jpeg')
+lemonade_kush.save!
+
+mojito_kush = Flower.new(
   name: "Mojito Kush",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -276,7 +341,12 @@ mojito_kush = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-peach_kush = Flower.create(
+mojito_kush_path = Rails.root.join('app', 'assets', 'images', 'mojito_kush.jpeg')
+mojito_kush_photo = File.open(mojito_kush_path)
+mojito_kush.photo.attach(io: mojito_kush_photo, filename: 'mojito_kush.jpeg', content_type: 'image/jpeg')
+mojito_kush.save!
+
+peach_kush = Flower.new(
   name: "Peach Kush",
   price: Faker::Commerce.price(range: 50.0..100.0),
   country: Faker::Address.country,
@@ -288,7 +358,12 @@ peach_kush = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 5),
   user: MERCHANT_USER
 )
-og_clementine = Flower.create(
+peach_kush_path = Rails.root.join('app', 'assets', 'images', 'peach_kush.jpeg')
+peach_kush_photo = File.open(peach_kush_path)
+peach_kush.photo.attach(io: peach_kush_photo, filename: 'peach_kush.jpeg', content_type: 'image/jpeg')
+peach_kush.save!
+
+og_clementine = Flower.new(
   name: "OG Clementine",
   price: Faker::Commerce.price(range: 50.0..100.0),
   country: Faker::Address.country,
@@ -300,7 +375,12 @@ og_clementine = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 5),
   user: MERCHANT_USER
 )
-key_lime_pie = Flower.create(
+og_clementine_path = Rails.root.join('app', 'assets', 'images', 'og_clementine.jpeg')
+og_clementine_photo = File.open(og_clementine_path)
+og_clementine.photo.attach(io: og_clementine_photo, filename: 'og_clementine.jpeg', content_type: 'image/jpeg')
+og_clementine.save!
+
+key_lime_pie = Flower.new(
   name: "Key Lime Pie",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -312,7 +392,12 @@ key_lime_pie = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
-lemon_meringue = Flower.create(
+key_lime_pie_path = Rails.root.join('app', 'assets', 'images', 'key_lime_pie.jpeg')
+key_lime_pie_photo = File.open(key_lime_pie_path)
+key_lime_pie.photo.attach(io: key_lime_pie_photo, filename: 'key_lime_pie.jpeg', content_type: 'image/jpeg')
+key_lime_pie.save!
+
+lemon_meringue = Flower.new(
   name: "Lemon Meringue",
   price: Faker::Commerce.price(range: 10.0..20.0),
   country: Faker::Address.country,
@@ -324,6 +409,45 @@ lemon_meringue = Flower.create(
   weight: Faker::Measurement.metric_weight(amount: 1),
   user: MERCHANT_USER
 )
+lemon_meringue_path = Rails.root.join('app', 'assets', 'images', 'lemon_meringue.jpeg')
+lemon_meringue_photo = File.open(lemon_meringue_path)
+lemon_meringue.photo.attach(io: lemon_meringue_photo, filename: 'lemon_meringue.jpeg', content_type: 'image/jpeg')
+lemon_meringue.save!
+
+ak_020 = Flower.new(
+  name: "Ak 020",
+  price: Faker::Commerce.price(range: 10.0..20.0),
+  country: Faker::Address.country,
+  category: Faker::Cannabis.type,
+  description: Faker::Lorem.paragraph(sentence_count: 6),
+  cbd_ratio: Faker::Number.within(range: 3.0..10.0),
+  effect: Faker::Cannabis.medical_use,
+  flavor: Faker::Dessert.flavor,
+  weight: Faker::Measurement.metric_weight(amount: 1),
+  user: MERCHANT_USER
+)
+ak_020_path = Rails.root.join('app', 'assets', 'images', 'ak_020.jpeg')
+ak_020_photo = File.open(ak_020_path)
+ak_020.photo.attach(io: ak_020_photo, filename: 'ak_020.jpeg', content_type: 'image/jpeg')
+ak_020.save!
+
+chunky_cookies = Flower.create(
+  name: "Chunky Cookies",
+  price: Faker::Commerce.price(range: 10.0..20.0),
+  country: Faker::Address.country,
+  category: Faker::Cannabis.type,
+  description: Faker::Lorem.paragraph(sentence_count: 6),
+  cbd_ratio: Faker::Number.within(range: 3.0..10.0),
+  effect: Faker::Cannabis.medical_use,
+  flavor: Faker::Dessert.flavor,
+  weight: Faker::Measurement.metric_weight(amount: 1),
+  user: MERCHANT_USER
+)
+chunky_cookies_path = Rails.root.join('app', 'assets', 'images', 'chunky_cookies.jpeg')
+chunky_cookies_photo = File.open(chunky_cookies_path)
+chunky_cookies.photo.attach(io: chunky_cookies_photo, filename: 'chunky_cookies.jpeg', content_type: 'image/jpeg')
+chunky_cookies.save!
+
 puts "created #{Flower.count} flowers"
 puts "Finished!"
 
