8a18abc5b10df51bf8c4ff21415ec40ac93524d3
elliott gabison <elliott.gabison@gmail.com>
2022-01-20 21:19:02 +0100

    add new logo, add page cbd world + maj assets doc

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: "pages#home"
+  get 'cbd_story', to: 'pages#cbd_story'
+  get 'contact', to: 'pages#contact'
 
   resources :flowers do
     resources :orders, only: [:new, :create, :index]
