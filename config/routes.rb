a2136f04331b108a58baed7be839244e4141b726
leo kassabi <leokassabi@gmail.com>
2022-01-22 11:16:11 +0100

    button

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
     resources :orders, only: [:new, :create, :index]
     resources :reviews, only: [:index, :show, :new, :create, :destroy, :update]
   end
-  resources :orders, only: [:show, :update, :index, :destroy]
+  resources :orders, only: [:show, :update, :create, :index, :destroy]
   resources :users, only: [:show] do
     member do
       get :sales
